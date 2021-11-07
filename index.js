3376134b1156816db8673ceed1f2bfa5df7968a4
m-triple-m <triplem656@gmail.com>
2021-11-08 00:03:10 +0530

    Merge branch 'master' of https://github.com/Mariyam2724/videoshare-backend

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const userRouter = require('./routers/userManager')
-const videoRouter = require('./routers/videoManager')
-const utilRouter = require('./routers/utils');
+const userRouter = require("./routers/userManager");
+const videoRouter = require("./routers/videoManager");
+const utilRouter = require("./routers/utils");
 
-const cors = require('cors');
-app.use(cors());
+const cors = require("cors");
+app.use(cors({ origin: ["http://localhost:4200"] }));
 app.use(express.json());
-app.use('/user', userRouter);
-app.use('/video', videoRouter);
-app.use('/util', utilRouter);
-app.use(express.static('./uploads'));
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
+app.use("/util", utilRouter);
+app.use(express.static("./uploads"));
 
-app.get('/home', (req, res) => {
-    console.log('recieve request');
-    res.send('Welcome Home Sweet Home');
-})
-app.listen(port, () => { console.log('server started ' + port) });
+app.get("/home", (req, res) => {
+  console.log("recieve request");
+  res.send("Welcome Home Sweet Home");
+});
+app.listen(port, () => {
+  console.log("server started " + port);
+});
